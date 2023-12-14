4a5c754f69cd08897e3a6c49b03bd1d041eb724b
musaomark01 <musaomark01@gmail.com>
2023-12-14 17:41:40 -0500

    add the api calls

---
+++ controllers/thoughtController.js
@@ -0,0 +1,101 @@
+const {Thought, User} = require('../models');
+
+module.exports = {
+    async getAllThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find({})
+            res.json(thoughts)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async getThoughtById(req, res) {
+        try {
+            const thought = await Thought.findOne({_id: req.params.thoughtId})
+            .select('-__v')
+            if (!thought) {
+                return res.status(404).json({message: 'No thought found with this id!'})
+            }
+            res.json(thought)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body)
+            const user = await User.findOneAndUpdate(
+                {_id: req.body.userId},
+                {$addToSet: {thoughts: thought._id}},
+                {new: true}
+            )
+            if (!user) {
+                return res.status(404).json({message: 'No user found with this id!'})
+            }
+            res.json(thought)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                {_id: req.params.thoughtId},
+                {$set: req.body},
+                {new: true, runValidators: true}
+            );
+            if (!thought) {
+                return res.status(404).json({message: 'No thought found with this id!'})
+            }
+            res.json(thought)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({_id: req.params.thoughtId})
+            if (!thought) {
+                return res.status(404).json({message: 'No thought found with this id!'})
+            }
+            await User.findOneAndUpdate(
+                {thoughts: req.params.thoughtId},
+                {$pull: {thoughts: req.params.thoughtId}},
+                {new: true}
+            )
+            res.json(thought)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async addReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                {_id: req.params.thoughtId},
+                {$addToSet: {reactions: req.body}},
+                {new: true, runValidators: true}
+            )
+            if (!thought) {
+                return res.status(404).json({message: 'No thought found with this id!'})
+            }
+            res.json(thought)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async removeReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                {_id: req.params.thoughtId},
+                {$pull: {reactions: {reactionId: req.params.reactionId}}},
+                {new: true}
+            )
+            if (!thought) {
+                return res.status(404).json({message: 'No thought found with this id!'})
+            }
+            res.json(thought)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+};
---
+++ controllers/userController.js
@@ -0,0 +1,89 @@
+const {User, Thought} = require('../models');
+
+module.exports = {
+    async getAllUsers(req, res) {
+        try {
+            const users = await User.find({})
+            res.json(users)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async getUserById(req, res) {
+        try {
+            const user = await User.findOne({_id: req.params.id})
+            .select('-__v')
+            if (!user) {
+                return res.status(404).json({message: 'No user found with this id!'})
+            }
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body)
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId},
+                {$set: req.body},
+                {new: true, runValidators: true}
+            );
+            if (!user) {
+                return res.status(404).json({message: 'No user found with this id!'})
+            }
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({_id: req.params.userId})
+            if (!user) {
+                return res.status(404).json({message: 'No user found with this id!'})
+            }
+            await Thought.deleteMany({_id: {$in: user.thoughts}})
+            res.json({message: 'User and associated thoughts deleted!'})
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async addFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId},
+                {$addToSet: {friends: req.params.friendId}},
+                {new: true}
+            )
+            if (!user) {
+                return res.status(404).json({message: 'No user found with this id!'})
+            }
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async removeFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId},
+                {$pull: {friends: req.params.friendId}},
+                {new: true}
+            )
+            if (!user) {
+                return res.status(404).json({message: 'No user found with this id!'})
+            }
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+};
